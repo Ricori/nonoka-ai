4e05e2677803181865c3d94377f97d980adfdd58
Ricori <17014@163.com>
2026-04-16 22:23:33 +0900

    feat: tts probability

--- src/modules/aiReply/group/index.ts
+++ src/modules/aiReply/group/index.ts
@@ -55,7 +55,7 @@ async function processReplyQueue(groupId: number, isInitiativeReply = false) {
       const aiReplyMessageParam = formatAssistantMessage(aiReplyText);
       messageStorage.addGroupChatConversations(groupId, aiReplyMessageParam);
 
-      if (Math.random() < 0.55) {
+      if (Math.random() < 0.3) {
         // 语音发送
         const message = aiReplyText.replace(/\[表情:\s*(.*?)\]/g, '').replace('||', '').trim();
         if (message) {
