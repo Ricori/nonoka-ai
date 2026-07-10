be71b944f9f74e33282a1b9d9cd52d502efb2ecd
Ricori <17014@163.com>
2026-07-11 03:50:25 +0900

    feat: ai

--- src/modules/aiReply/format.ts
+++ src/modules/aiReply/format.ts
@@ -36,7 +36,7 @@ export function formatMessage(
   let isMentionMe = rawMessage.indexOf(`[CQ:at,qq=${selfId}]`) > -1;
 
   // 包含名字也算被提到
-  if (rawMessage.includes('乃乃香')) {
+  if (rawMessage.includes('乃乃香') || rawMessage.includes('nonoka') || rawMessage.includes('nnk')) {
     isMentionMe = true;
   }
 
