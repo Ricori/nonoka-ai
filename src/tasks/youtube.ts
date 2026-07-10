f3bceebcc51163f1a5f06691ffa9cd627049228a
Ricori <17014@163.com>
2026-07-10 22:16:38 +0900

    feat: youtube

--- src/tasks/youtube.ts
+++ src/tasks/youtube.ts
@@ -20,10 +20,11 @@ async function checkYtLive(channelName: string, groupIds: number[]) {
 
     const msgTextArr = [] as string[];
     msgTextArr.push(`${status.title ?? '直播'} 开始了！`);
-    if (status.thumbnail) msgTextArr.push(getImgCode(status.thumbnail));
+    if (status.thumbnail) msgTextArr.push(getImgCode(status.thumbnail.replace('i.ytimg.com', 'cdn.nonoka.online/yt/img')));
     msgTextArr.push(`直播链接：https://www.youtube.com/watch?v=${status.videoId}`);
+    msgTextArr.push(`实时翻译：https://live.nonoka.online/live?channel=@${channelName}`);
     const msg = msgTextArr.join('\n');
-
+    printLog(`[ytLiveTask] Pushing live notification for channel ${channelName} to groups: ${groupIds.join(', ')}`);
     groupIds.forEach((groupId) => {
       nnkbot.sendGroupMsg(groupId, msg);
     });
@@ -45,7 +46,7 @@ const task = new AsyncTask('ytLiveTask', async () => {
   }
 });
 
-const YtLivePushJob = new SimpleIntervalJob({ seconds: 90 }, task, { id: 'ytLivePush', preventOverrun: true });
+const YtLivePushJob = new SimpleIntervalJob({ seconds: 10 }, task, { id: 'ytLivePush', preventOverrun: true });
 
 // 启动 bot 时预先拉取一次当前直播状态，避免重启时把正在进行中的直播当作新开播重复推送
 Object.keys(nnkbot.config.ytLivePush.config).forEach((channelName: string) => {
